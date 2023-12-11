187879472201e3449e3b3773f56acc6250115b9b
sadakhalil <s.halil@yahoo.com>
2023-12-11 14:18:49 +0100

    refactored and solved part 2

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -1,58 +1,74 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"strconv"
-	"unicode"
 )
 
-func processLine(line string) string {
+func processLine(line []byte) int {
 
-	var firstDigit, secondDigit rune
-
-	//can left to right
-	for _, char := range line {
-		if unicode.IsDigit(char) {
-			firstDigit = char
-			break
-		}
+	var firstDigit, secondDigit byte
+	digits := []byte("123456789")
+	words := map[string]byte{
+		"one":   '1',
+		"two":   '2',
+		"three": '3',
+		"four":  '4',
+		"five":  '5',
+		"six":   '6',
+		"seven": '7',
+		"eight": '8',
+		"nine":  '9',
 	}
 
-	//scan right to left
-	for i := len(line) - 1; i >= 0; i-- {
-		if unicode.IsDigit(rune(line[i])) {
-			secondDigit = rune(line[i])
-			break
+	l := len(line)
+
+	for i, r := range line {
+		if bytes.Contains(digits, []byte{r}) {
+			if firstDigit == 0 {
+				firstDigit = r
+			}
+			secondDigit = r
+			continue
+		}
+		for key, value := range words {
+			if r == key[0] {
+				if i+len(key)-1 > l-1 {
+					continue
+				}
+
+				if !bytes.Equal(line[i:i+len(key)], []byte(key)) {
+					continue
+				}
+
+				if firstDigit == 0 {
+					firstDigit = value
+				}
+
+				secondDigit = value
+				break
+
+			}
 		}
 	}
-
-	return string(firstDigit) + string(secondDigit)
-
+	i, _ := strconv.Atoi(string(firstDigit) + string(secondDigit))
+	return i
 }
 
 func main() {
 
 	var answer int = 0
 
-	file, err := os.Open("input.txt")
+	file, err := os.ReadFile("input.txt")
 	if err != nil {
-		fmt.Println("Error opening the input file", err)
+		fmt.Println("Error reading the file", err)
 		return
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		number, _ := strconv.Atoi(processLine(line))
-		answer += number
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading from file", err)
+	for _, line := range bytes.Split(file, []byte("\n")) {
+		answer += processLine(line)
 	}
 
 	fmt.Println(answer)

4871a2f19238de3eafdf7112dc6563433b27cba8
sadakhalil <s.halil@yahoo.com>
2023-12-24 14:25:50 +0200

    day4part1 solved

---
+++ 2023/4/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"os"
+)
+
+func cleanLine(line []byte) []byte {
+	idx := bytes.Index(line, []byte(": "))
+	line = line[idx+2:]
+	return line
+}
+
+func splitNumbers(line []byte) (winningNumbers []byte, myNumbers []byte) {
+	idx := bytes.Index(line, []byte(" | "))
+	winningNumbers = line[:idx]
+	myNumbers = line[idx+3:]
+	return winningNumbers, myNumbers
+}
+
+func extractNumbers(slice []byte) []int {
+	var numbers []int
+	for _, word := range bytes.Fields(slice) {
+		var number int
+		for i := 0; i < len(word); i++ {
+			number = number*10 + int(word[i]-'0')
+		}
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func winningNumbersCount(line []byte) int {
+	winningNumbers, myNumbers := splitNumbers(line)
+	winningNumbersInt := extractNumbers(winningNumbers)
+	myNumbersInt := extractNumbers(myNumbers)
+
+	var winningNumberCount int = 0
+
+	for _, winningNumber := range winningNumbersInt {
+		for _, myNumber := range myNumbersInt {
+			fmt.Println("winning: ", winningNumber, " my: ", myNumber, " count: ", winningNumberCount)
+			if winningNumber == myNumber {
+				winningNumberCount++
+			}
+
+		}
+	}
+	return winningNumberCount
+}
+
+func main() {
+
+	//read file input
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading from file", err)
+		return
+	}
+
+	data := bytes.Split(input, []byte("\n"))
+
+	result := 0
+
+	for _, line := range data {
+		line = cleanLine(line)
+
+		winningNumbers := winningNumbersCount(line)
+		fmt.Println(winningNumbers)
+
+		if winningNumbers != 0 {
+			lineScore := math.Pow(2, float64(winningNumbers)-1)
+			result += int(lineScore)
+		}
+	}
+
+	fmt.Println(result)
+
+}
